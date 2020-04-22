55c5c2443381da4982c6cffbe8741f3ca33c934d
aaaa.aaaa <aaaa@aaa.aaa>
2020-04-22 14:40:56 +0900

    added Board, Article, Reply Mappers

---
+++ wcy/src/main/java/com/it/wecodeyou/board/repository/IBoardMapper.java
@@ -0,0 +1,19 @@
+package com.it.wecodeyou.board.repository;
+
+import java.util.List;
+
+import com.it.wecodeyou.board.model.BoardVO;
+
+public interface IBoardMapper {
+	//게시판 삽입문
+	public void insert(BoardVO rvo);
+
+	//게시판 삭제
+	public void delete(BoardVO rvo);
+	
+	//게시판 수정
+	public void update(BoardVO rvo);
+	
+	//게시판 리스트
+	public List<BoardVO> list();
+}
