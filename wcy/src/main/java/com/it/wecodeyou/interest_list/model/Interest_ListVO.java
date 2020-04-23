4839f4567d040ab382adaf60c0b2b9dd2af6f5de
USER <USER@DESKTOP-PM6I5RF>
2020-04-23 12:10:57 +0900

    haejin12:10 add interest

--- wcy/src/main/java/com/it/wecodeyou/interest_list/model/Interest_ListVO.java
+++ wcy/src/main/java/com/it/wecodeyou/interest_list/model/Interest_ListVO.java
@@ -4,15 +4,20 @@ public class Interest_ListVO {
 	
 	private String interestListId;
 	private String interestListType;
-	private String interestListName;
+	private String interestListAnswer;
+	private String interestListQuestion;
+	private Integer interestListOption;
 	
 	public Interest_ListVO() {}
 
-	public Interest_ListVO(String interestListId, String interestListType, String interestListName) {
+	public Interest_ListVO(String interestListId, String interestListType, String interestListAnswer,
+			String interestListQuestion, Integer interestListOption) {
 		super();
 		this.interestListId = interestListId;
 		this.interestListType = interestListType;
-		this.interestListName = interestListName;
+		this.interestListAnswer = interestListAnswer;
+		this.interestListQuestion = interestListQuestion;
+		this.interestListOption = interestListOption;
 	}
 
 	public String getInterestListId() {
@@ -31,19 +36,38 @@ public class Interest_ListVO {
 		this.interestListType = interestListType;
 	}
 
-	public String getInterestListName() {
-		return interestListName;
+	public String getInterestListAnswer() {
+		return interestListAnswer;
 	}
 
-	public void setInterestListName(String interestListName) {
-		this.interestListName = interestListName;
+	public void setInterestListAnswer(String interestListAnswer) {
+		this.interestListAnswer = interestListAnswer;
+	}
+
+	public String getInterestListQuestion() {
+		return interestListQuestion;
+	}
+
+	public void setInterestListQuestion(String interestListQuestion) {
+		this.interestListQuestion = interestListQuestion;
+	}
+
+	public int getInterestListOption() {
+		return interestListOption;
+	}
+
+	public void setInterestListOption(int interestListOption) {
+		this.interestListOption = interestListOption;
 	}
 
 	@Override
 	public String toString() {
 		return "Interest_ListVO [interestListId=" + interestListId + ", interestListType=" + interestListType
-				+ ", interestListName=" + interestListName + "]";
+				+ ", interestListAnswer=" + interestListAnswer + ", interestListQuestion=" + interestListQuestion
+				+ ", interestListOption=" + interestListOption + "]";
 	}
+
 	
+
 	
 }
--- wcy/src/main/java/com/it/wecodeyou/interest_list/service/IInterest_ListService.java
+++ wcy/src/main/java/com/it/wecodeyou/interest_list/service/IInterest_ListService.java
@@ -8,7 +8,9 @@ import com.it.wecodeyou.interest_list.model.Interest_ListVO;
 public interface IInterest_ListService {
 
 	// 전체 타입 목록 출력 (String 리스트 ex. "lang", "period"...)
-	public ArrayList<String> getOneInterestType() throws SQLException;
+	public ArrayList<Interest_ListVO> getOneInterestType() throws SQLException;
+	
+	//
 
 	// 전체 interst_list 정보 출력
 	public ArrayList<Interest_ListVO> getAllInterestList() throws SQLException;
